2b07ebd54d75d91278fdad77a71e11e9640182d0
Miya096jp <miyawaki.yske@gmail.com>
2026-01-30 17:56:09 +0900

    Skip OmniAuth config if credentials are missing
    to fix assets precompilation failure in Docker build

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -315,12 +315,17 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :github,
-    Rails.application.credentials.github[:client_id],
-    Rails.application.credentials.github[:client_secret],
-    scope: "user:email"
-  config.omniauth :google_oauth2,
-    Rails.application.credentials.google[:client_id],
-    Rails.application.credentials.google[:client_secret],
-    scope: "email,profile"
+  if Rails.application.credentials.github.present?
+    config.omniauth :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret],
+      scope: "user:email"
+  end
+
+  if Rails.application.credentials.github.present?
+    config.omniauth :google_oauth2,
+      Rails.application.credentials.google[:client_id],
+      Rails.application.credentials.google[:client_secret],
+      scope: "email,profile"
+  end
 end
