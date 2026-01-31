a7cb5c4eff11835e4dc31001f4b124ed18c4f74f
Miya096jp <miyawaki.yske@gmail.com>
2026-01-31 14:07:11 +0900

    Add SES smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -68,6 +68,11 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+
+  # config/environments/production.rb
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Rails.application.credentials.production[:smtp]
+  config.action_mailer.default_url_options = { host: "dualpascal.com", protocol: "https" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
